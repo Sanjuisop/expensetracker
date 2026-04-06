1589a93bd51764811901d878c30922236008c1e4
raymanaly <manalyray07@gmail.com>
2026-04-07 00:24:45 +0530

    Changed currency to Rupee and fixed JS logic

--- script.js
+++ script.js
@@ -11,17 +11,21 @@ let transactions = localStorage.getItem('transactions') !== null ? localStorageT
 
 function addTransaction(e) {
   e.preventDefault();
-  const transaction = {
-    id: Math.floor(Math.random() * 100000000),
-    text: text.value,
-    amount: +amount.value
-  };
-  transactions.push(transaction);
-  addTransactionDOM(transaction);
-  updateValues();
-  updateLocalStorage();
-  text.value = '';
-  amount.value = '';
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a description and amount');
+  } else {
+    const transaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text: text.value,
+      amount: +amount.value
+    };
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+    text.value = '';
+    amount.value = '';
+  }
 }
 
 function addTransactionDOM(transaction) {
@@ -29,7 +33,8 @@ function addTransactionDOM(transaction) {
   const item = document.createElement('li');
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
   item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+    ${transaction.text} <span>${sign}₹${Math.abs(transaction.amount).toFixed(2)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
   `;
   list.appendChild(item);
 }
@@ -40,14 +45,26 @@ function updateValues() {
   const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
   const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
 
-  balance.innerText = `$${total}`;
-  money_plus.innerText = `+$${income}`;
-  money_minus.innerText = `-$${expense}`;
+  balance.innerText = `₹${total}`;
+  money_plus.innerText = `+₹${income}`;
+  money_minus.innerText = `-₹${expense}`;
+}
+
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+  updateLocalStorage();
+  init();
 }
 
 function updateLocalStorage() {
   localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
 form.addEventListener('submit', addTransaction);
-window.onload = () => transactions.forEach(addTransactionDOM);
